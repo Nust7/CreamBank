76fcfc800710004dd69ff7934e524770dce525f9
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2024-03-17 14:54:47 -0300

    Corrigindo formulario e funcao de envio

--- src/script.js
+++ src/script.js
@@ -1,33 +1,32 @@
 function enviarFormulario() {
-    // Obtém os dados do formulário
-    var formulario = document.getElementById("meuFormulario");
-    var dadosFormulario = new FormData(formulario);
-  
-    // Converte os dados do formulário para um objeto JSON
-    var objetoJSON = {};
-    dadosFormulario.forEach(function(valor, chave){
-      objetoJSON[chave] = valor;
-    });
-  
-    // Envia o objeto JSON para a API (substitua a URL pela sua API)
-    fetch('sua_api.com/recurso', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(objetoJSON)
-    })
-    .then(function(response) {
-      if (response.ok) {
-        alert('Formulário enviado com sucesso!');
-        // Faça qualquer outra ação necessária após o envio do formulário
-      } else {
-        alert('Erro ao enviar o formulário. Por favor, tente novamente.');
-      }
-    })
-    .catch(function(error) {
-      console.error('Erro durante a solicitação:', error);
-      alert('Erro durante o envio do formulário. Por favor, tente novamente mais tarde.');
-    });
-  }
-  
+  // Obtém os dados do formulário
+  var formulario = document.getElementById("form-cadastrar");
+  var dadosFormulario = new FormData(formulario);
+
+  // Converte os dados do formulário para um objeto JSON
+  var objetoJSON = {};
+  dadosFormulario.forEach(function(valor, chave){
+    objetoJSON[chave] = valor;
+  });
+
+  // Envia o objeto JSON para a API (substitua a URL pela sua API)
+  fetch('http://localhost:8080/cliente/cadastrarTeste', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json' // Configura o tipo de mídia como JSON
+    },
+    body: JSON.stringify(objetoJSON) // Converte o objeto JSON para uma string JSON
+  })
+  .then(function(response) {
+    if (response.ok) {
+      alert('Formulário enviado com sucesso!');
+      // Faça qualquer outra ação necessária após o envio do formulário
+    } else {
+      alert('Erro ao enviar o formulário. Por favor, tente novamente.');
+    }
+  })
+  .catch(function(error) {
+    console.error('Erro durante a solicitação:', error);
+    alert('Erro durante o envio do formulário. Por favor, tente novamente mais tarde.');
+  });
+}
