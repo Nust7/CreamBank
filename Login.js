cdad986366aa999b39e5443f2acde1789b96033c
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2024-03-20 23:32:27 -0300

    Login - Enviando informacoes do cliente para a proxima tela.

---
+++ Login.js
@@ -0,0 +1,5 @@
+function cliente (){
+    console.log("Chegou no cliente")
+    // const car = {"numeroConta": data.numeroConta, "nome": data.nome, "saldo": data.saldo};
+    // window.location.href = 'login.html';
+}
