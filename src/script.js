2c4659c457739c4b58db376e2542a3b6a8c91660
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2024-03-23 19:18:22 -0300

    Criando funcionalidade de transferencia de dinheiro

--- src/script.js
+++ src/script.js
@@ -37,3 +37,43 @@ function enviarFormulario() {
     alert('Erro durante o envio do formulário. Por favor, tente novamente mais tarde.');
   });
 }
+
+
+function transferir(){
+  console.log("aqui")
+  const transferencia = document.getElementById("formulario-transferencia");
+  const dadosTransferencia = new FormData(transferencia);
+
+  let dadosCliente = JSON.parse(localStorage.getItem("dadosCliente"));
+  console.log("Local storage " + localStorage.getItem("dadosCliente"))
+  numeroConta = dadosCliente.numeroConta;
+
+  console.log("numero"+ numeroConta)
+  dadosTransferencia.append("contaRemetente", numeroConta);
+
+  const objetoJSON = {};
+  dadosTransferencia.forEach(function(valor, chave){
+    objetoJSON[chave] = valor;
+  });
+  console.log("json "+ objetoJSON)
+
+  fetch('http://localhost:8080/transacoes/enviarDinheiro', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json' // Configura o tipo de mídia como JSON
+    },
+    body: JSON.stringify(objetoJSON) // Converte o objeto JSON para uma string JSON
+  })
+      .then(function(response) {
+        if (response.ok) {
+          alert('Transferência enviado com sucesso!');
+          // Faça qualquer outra ação necessária após o envio do formulário
+        } else {
+          alert('Erro ao enviar o Transferência. Por favor, tente novamente.');
+        }
+      })
+      .catch(function(error) {
+        console.error('Erro durante a solicitação:', error);
+        alert('Erro durante o envio da Transferência. Por favor, tente novamente mais tarde.');
+      });
+}
